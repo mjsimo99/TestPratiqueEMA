da33ab48d6273faada51d773f83640c4dc01b047
mjsimo99 <mohashark21@gmail.com>
2024-04-25 17:58:31 -0700

    setup my project

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+
+require_once './autoload.php';
+include('./views/includes/alerts.php');
+
+$home = new HomeController();
+$pages = ['index','country','search'];
+
+
+if (isset($_GET['page'])) {
+    if (in_array($_GET['page'],$pages)) {
+        $page = $_GET['page'];
+        $home->index($page);
+    }else{
+        include('views/includes/404.php');
+    }
+}else{
+    $home->index('index');
+}
+
+
+require_once './views/includes/footer.php';
