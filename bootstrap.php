0a44fb63cee3650539b6ea71a975b5bb21216f1c
mjsimo99 <mohashark21@gmail.com>
2024-04-25 17:58:48 -0700

    create configs files

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+                                                        
+
+<?php
+define('BASE_URL', 'http://localhost/Satoru-MVC1/');
+?>
+                                                        
+                                                    
