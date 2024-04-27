e92523b64683d6f834a3954633c06b9bc75e03a9
mjsimo99 <mohashark21@gmail.com>
2024-04-26 18:50:46 -0700

    add adress controller

---
+++ Controllers/AddressController.php
@@ -0,0 +1,16 @@
+<?php
+// app/Controllers/AddressController.php
+class AddressController
+{
+    private $addressModel;
+
+    public function __construct(AddressModel $addressModel)
+    {
+        $this->addressModel = $addressModel;
+    }
+
+    public function searchNdiByAddress(?float $latitude = null, ?float $longitude = null)
+    {
+        return $this->addressModel->searchNdiByAddress($latitude, $longitude);
+    }
+}
