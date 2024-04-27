664f702b3795c9a535e1792cfc740bb470f2253e
mjsimo99 <mohashark21@gmail.com>
2024-04-26 18:50:33 -0700

    edit config

--- app/Classes/Config/config.php
+++ app/Classes/Config/config.php
@@ -2,3 +2,4 @@
 define('COUNTRY', 'https://demo-xvno.ema.expert/ema/api/v1/endpoint/country/');
 define('API_USERNAME', '14484');
 define('API_PASSWORD', '3fca31f5bee9e79e6951a1192c991e1fb05fdf9ebe912e4fbdfb6d5feb79663e525e7d36bd10ae271425f232b55b15e8e166bcc65f5d350509d9f5c4e5f8cad5');
+define('NDI_API_URL', 'https://demo-xvno.ema.expert/ema/api/v1/endpoint/ndi/');
