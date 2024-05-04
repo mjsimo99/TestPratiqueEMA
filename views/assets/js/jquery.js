fc3609e0f44a865d879f13aa97abb69319627635
mjsimo99 <mohashark21@gmail.com>
2024-05-03 20:01:20 -0700

    fix range

--- views/assets/js/jquery.js
+++ views/assets/js/jquery.js
@@ -1,23 +1,25 @@
-$(document).ready(function() {
-    function updateSpeedValue() {
-      const value = $("#speed-range").val();
-      const speed = value < 1000 ? `${value} MB/s` : `${(value / 1000).toFixed(2)} GB/s`;
-      $("#speed-value").text(speed);
-    }
-  
-    $("#speed-range").on("input", function() {
-      updateSpeedValue();
-    });
-  
-    updateSpeedValue();
+$(document).ready(function(){
+  $('.speed-range').each(function() {
+      var maxValue = parseInt($(this).attr('max'));
+      var stepSize = maxValue / 1000;
+      $(this).attr('max', maxValue);
+      $(this).attr('step', stepSize);
+      $(this).on('input', function() {
+          var value = $(this).val();
+          var speed = formatSpeed(value);
+          $(this).closest('.td').find('.selected-range').text(speed);
+      });
   });
 
+  function formatSpeed(value) {
+      if (value >= 1000) {
+          return Math.round(value / 1000) + " Gb/s";
+      } else {
+          return Math.round(value) + " Mb/s";
+      }
+  }
+});
 
 
 
-    $(document).ready(function() {
-        $('input[type="radio"]').change(function() {
-            var selectedOption = $(this).val();
-            $(this).closest('td').text(selectedOption);
-        });
-    });
+
