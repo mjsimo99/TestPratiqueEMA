104731a03ef5c3e4cb97f066de913a6d46d00f62
mjsimo99 <mohashark21@gmail.com>
2024-05-03 20:01:12 -0700

    new update to fetch data count

--- views/ndiResult.php
+++ views/ndiResult.php
@@ -1,4 +1,3 @@
-<!-- ndiresult.php -->
 <?php
 
 require './views/includes/header.php';
@@ -24,13 +23,13 @@ $eligibilityController = new EligibilityController($eligibilityModel);
 
 foreach ($ndiList as $ndiItem) {
 
-$ndi = $ndiData['data']['ndi'];
-$ndiStatus = $ndiData['data']['ndi_status'];
-$idtown = $ndiItem['idtown'];
-$street = $ndiItem['street'];
-$number = $ndiItem['street_number'];
-$latitude = $ndiItem['latitude'];
-$longitude = $ndiItem['longitude'];
+    $ndi = $ndiData['data']['ndi'];
+    $ndiStatus = $ndiData['data']['ndi_status'];
+    $idtown = $ndiItem['idtown'];
+    $street = $ndiItem['street'];
+    $number = $ndiItem['street_number'];
+    $latitude = $ndiItem['latitude'];
+    $longitude = $ndiItem['longitude'];
 }
 
 $data = $eligibilityController->fetchData($ndi, $ndiStatus, $idtown, $street, $number, $latitude, $longitude);
@@ -69,125 +68,163 @@ $data = $eligibilityController->fetchData($ndi, $ndiStatus, $idtown, $street, $n
                 </div>
             </div>
             <div>
-            <div class="informaion-tech">
-                <div class="info-b">
-                    <h2 class="table-title">Informations techniques</h2>
-                    <div class="line"></div>
+                <div class="informaion-tech">
+                    <div class="info-b">
+                        <h2 class="table-title">Informations techniques</h2>
+                        <div class="line"></div>
+                    </div>
+                    <div>
+                        <p>Site : <?= $ndiData['data']['site'] ?? 'Non fibré Orange' ?></p>
+                    </div>
                 </div>
-                <div>
-                    <p>Site : <?= $ndiData['data']['site'] ?? 'Non fibré Orange' ?></p>
+            </div>
+        <?php else : ?>
+            <p>No NDI data found for the provided phone number.</p>
+        <?php endif; ?>
+
+        </div>
+
+        <div class="partie-two">
+            <div class="next-p">
+                <p class="p"> <span class="promo">promo</span> Formeture rèseau cuivre : promotion de migration vers la fibre jusqu'au 31/12/2023</p>
+            </div>
+
+            <div class="right-side">
+                <div class="circle">
+                    <span class="number">100 %</span>
+                </div>
+                <div class="offre-eli">
+                    <p class="p-size n-a">offre éligibles <span class="n-offres">
+                            <?php
+                            $offerCount = 0;
+                            foreach ($data as $section) {
+                                if (isset($section['list'])) {
+                                    foreach ($section['list'] as $mainList) {
+                                        if (isset($mainList['list'])) {
+                                            foreach ($mainList['list'] as $offerList) {
+                                                if (isset($offerList['list'])) {
+                                                    $offerCount += count($offerList['list']);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            echo $offerCount;
+                            ?>
+                        </span></p>
+                    <p class="p-size">tous réduire</p>
                 </div>
             </div>
         </div>
-    <?php else : ?>
-        <p>No NDI data found for the provided phone number.</p>
-    <?php endif; ?>
-    
-</div>
 
-<div class="partie-two">
-    <div class="next-p">
-    <p class="p"> <span class="promo">promo</span>  Formeture rèseau cuivre : promotion de migration vers la fibre jusqu'au 31/12/2023</p>
-    </div>
+        <div class="final-side">
+            <div class="title-p">
+                <div class="final-title">
+                    <h2>EXECUTIVE ACCESS</h2>
+                    <div class="line l-b"></div>
+                </div>
+                <div>
+                    <p class="final-p">
+                        reduire
+                    </p>
+                </div>
+            </div>
 
-    <div class="right-side">
-        <div class="circle">
-            <span class="number">100 %</span>
-        </div>
-        <div class="offre-eli">
-            <p class="p-size n-a">offre éligibles <span class="n-offres">10</span></p>
-            <p class="p-size">tous réduire 
-            </p>
-        </div>
-    </div>
-</div>
+            <table>
+                <thead>
+                    <tr class="table-header">
+                        <th class="th">Opérator</th>
+                        <th class="th">Offre</th>
+                        <th class="th">Début</th>
+                        <th class="th">Engagement</th>
+                        <th class="th">Abonnement</th>
+                        <th class="th">FAS</th>
+                        <th class="th"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $offerCount = 0;
+                    foreach ($data as $section) {
+                        if (isset($section['list'])) {
+                            foreach ($section['list'] as $mainList) {
+                                if (isset($mainList['list'])) {
+                                    foreach ($mainList['list'] as $offerList) {
+                                        if (isset($offerList['list'])) {
+                                            $offerCount += count($offerList['list']);
 
-<div class="final-side">
-    <div class="title-p">
-        <div class="final-title">
-            <h2>EXECUTIVE ACCESS</h2>
-            <div class="line l-b"></div>
-        </div>
-        <div >
-            <p class="final-p">
-            reduire
-            </p>
-        </div>
-    </div>
+                                            foreach ($offerList['list'] as $offer) {
+                                                echo "<tr>";
+                                                echo "<td class='td'>" . $offer['provider'] . "</td>";
+                                                echo "<td class='td'>" . $offer['offer'] . "</td>";
+                                                if (preg_match('/^(\d+)(?=Mb|Kb)/', $offer['debit'], $matches) && ($matches[1] <= 10 || $offer['debit'] == '512Kb')) {
+                                                    if ($offer['debit'] == '512Kb') {
+                                                        echo "<td class='td'>" . '512Kb/s' . "</td>";
+                                                    } else {
+                                                        $maxValue = preg_replace('/[^0-9]/', '', $offer['debit']);
+                                                        echo "<td class='td'>" . "<div class='td-range'>" .
+                                                            "<span>512 Kb/s</span>" .
+                                                            "<input type='range' class='speed-range' max='$maxValue' step='1'> " .
+                                                            "<span class='speed-value'>" . preg_replace('/\D/', '', $offer['debit']) . substr($offer['debit'], strlen(preg_replace('/\D/', '', $offer['debit'])), 2) . "/s" . "</span>" .
+                                                            "</div>" .
+                                                            "<br>" .
+                                                            "<span class='selected-range'></span>" .
+                                                            "</td>";
+                                                    }
+                                                } else {
+                                                    $maxValue = preg_replace('/[^0-9]/', '', $offer['debit']);
+                                                    echo "<td class='td'>" . "<div class='td-range'>" .
+                                                        "<span>10Mb/s</span>" .
+                                                        "<input type='range' class='speed-range' max='$maxValue' step='1'> " .
+                                                        "<span class='speed-value'>" . preg_replace('/\D/', '', $offer['debit']) . substr($offer['debit'], strlen(preg_replace('/\D/', '', $offer['debit'])), 2) . "/s" . "</span>" .
+                                                        "</div>" .
+                                                        "<br>" .
+                                                        "<span class='selected-range'></span>" .
+                                                        "</td>";
+                                                }
+                                                echo "<td class='td'>";
 
-<table>
-<thead>
-                <tr class="table-header">
-                    <th class="th">Opérator</th>
-                    <th class="th">Offre</th>
-                    <th class="th">Début</th>
-                    <th class="th">Engagement</th>
-                    <th class="th">Abonnement</th>
-                    <th class="th">FAS</th>
-                    <th class="th"></th>
-                </tr>
-            </thead>
-            <tbody>
-        <?php
-        $offerCount = 0;
-        foreach ($data as $section) {
-            if (isset($section['list'])) {
-                foreach ($section['list'] as $mainList) {
-                    if (isset($mainList['list'])) {
-                        foreach ($mainList['list'] as $offerList) {
-                            if (isset($offerList['list'])) {
-                                $offerCount += count($offerList['list']);
+                                                $engagementOptions = explode(' ', $offer['engagement']);
+                                                $engagementValue = (int) $engagementOptions[0];
+                                                $engagementUnit = isset($engagementOptions[1]) ? $engagementOptions[1] : '';
 
-                                foreach ($offerList['list'] as $offer) {
-                                    echo "<tr>";
-                                    echo "<td class='td'>" . $offer['provider'] . "</td>";
-                                    echo "<td class='td'>" . $offer['offer'] . "</td>";
-                                    echo "<td class='td'>"
-                                        . "<div class='td-range'>"
-                                        . "<span>10 Mb/s</span>"
-                                        . "<input type='range' id='speed-range' min='10' max='10000' value='100' step='1'> "
-                                        . "<span id='speed-value'>" . $offer['debit'] ."</span>"
-                                        . "</div>"
-                                        . "</td>";
-                                    echo "<td class='td'>";
+                                                if ($offer['engagement'] === "Sans engagement") {
+                                                    echo $offer['engagement'];
+                                                } else {
 
-                                        $engagementOptions = explode(' ', $offer['engagement']);
-                                        $engagementValue = (int) $engagementOptions[0];
-                                        $engagementUnit = isset($engagementOptions[1]) ? $engagementOptions[1] : '';
-                                        
-                                        if ($offer['engagement'] === "Sans engagement") {
-                                            echo $offer['engagement'];
-                                        } else {
-                                        
-                                            for ($i = 1; $i <= $engagementValue; $i++) {
-                                                echo "<label><input type='radio' name='engagement-option' value='$i $engagementUnit'> $i $engagementUnit</label><br>";
+                                                    for ($i = 1; $i <= $engagementValue; $i++) {
+                                                        echo "<label><input type='radio' name='engagement-option' value='$i $engagementUnit'> $i $engagementUnit</label><br>";
+                                                    }
+                                                }
+
+                                                echo "</td>";
+                                                echo "</td>";
+                                                echo "<td class='td'>" . $offer['amt_recur'] . "€/Mois</td>";
+                                                echo "<td class='td'>" . 'sur devis' . "</td>";
+                                                echo "</tr>";
                                             }
                                         }
-                                        
-                                    echo "</td>";
-                                    echo "</td>";
-                                    echo "<td class='td'>" . $offer['amt_recur'] . "€/Mois</td>";
-                                    echo "<td class='td'>" . 'sur devis' . "</td>";
-                                    echo "</tr>";
+                                    }
                                 }
                             }
                         }
                     }
-                }
-            }
-        }
-        echo "<tr><td colspan='7'>Total offers: " . $offerCount . "</td></tr>";
-        ?>
-    </tbody>
+                    echo "<tr><td colspan='7'>Total offers: " . $offerCount . "</td></tr>";
+                    ?>
+                </tbody>
             </table>
-</div>
 
-<script>
-    const ndiList = <?php echo json_encode($ndiList); ?>;
-</script>
-<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"/>
-<script src="views/assets/js/map.js"></script>
+        </div>
 
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+            const ndiList = <?php echo json_encode($ndiList); ?>;
+        </script>
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" />
+        <script src="views/assets/js/map.js"></script>
+
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="views/assets/js/jquery.js"></script>
