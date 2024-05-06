ce01ed3a0a41d519583c786169162ee5e6a05b1d
mjsimo99 <mohashark21@gmail.com>
2024-05-05 17:37:05 -0700

    clean the code

--- views/index.php
+++ views/index.php
@@ -1,59 +1,5 @@
 <?php
-require_once 'app/Classes/config/config.php';
-
-$username = API_USERNAME;
-$password = API_PASSWORD;
-
-function setupCurl($url, $username, $password) {
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-    return $curl;
-}
-
-function fetchData($url, $username, $password) {
-    $curl = setupCurl($url, $username, $password);
-    $response = curl_exec($curl);
-    $error = curl_error($curl);
-    curl_close($curl);
-
-    if ($error) {
-        throw new RuntimeException("Curl error: $error");
-    }
-
-    return $response;
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
-    $action = $_POST['action'];
-
-    switch ($action) {
-        case 'fetchTowns':
-            $postalCode = $_POST['postalCode'];
-            $url = 'https://demo-xvno.ema.expert/ema/api/v1/endpoint/town/?iso_alpha2=fr&zip=' . $postalCode;
-            echo fetchData($url, $username, $password);
-            break;
-        case 'fetchStreets':
-            $idtown = $_POST['idtown'];
-            $streetName = $_POST['streetName'];
-            $url = 'https://demo-xvno.ema.expert/ema/api/v1/endpoint/street/?idtown=' . $idtown . '&street=' . urlencode($streetName);
-            echo fetchData($url, $username, $password);
-            break;
-        case 'fetchHouseNumbers':
-            $idtown = $_POST['idtown'];
-            $idstreet = $_POST['idstreet'];
-            $number = $_POST['number']; 
-            $url = 'https://demo-xvno.ema.expert/ema/api/v1/endpoint/housenumber/?iso_alpha2=fr&idtown=' . $idtown . '&idstreet=' . $idstreet . '&number=' . $number; // Update the URL
-            echo fetchData($url, $username, $password);
-            break;
-
-    }
-    exit;
-} else {
-    require './views/includes/header.php';
-}
+include '../Satoru-MVC1/views/data/apiClient.php';
 ?>
 
 
@@ -128,10 +74,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SERVER['HTTP_X_REQUESTED_WI
             </div>
           </div>
           <div class="input-group-b">
-              <!-- <div class="input-container">
-                <label class="search-labels">Nom de la voie</label>
-                <input type="text" id="street-input" placeholder="Ex: Gambetta" name="street-input">
-              </div> -->
               <div class="input-container">
                   <label class="search-labels">Nom de la voie</label>
                   <input type="text" id="street-input" placeholder="Ex: Gambetta" name="street-input" list="street-suggestions">
